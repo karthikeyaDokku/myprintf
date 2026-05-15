16d0a21776727aca34f1d0ee53b29cfd53ce72b4
karthikeya <karthikeya2601@gmail.com>
2026-05-15 15:54:31 +0530

    simple function that prints a string

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
 
+void my_printf(char *str)
+{
+    while (*str)
+    {
+        putchar(*str);
+        str++;
+    }
+}
+
+int main()
+{
+    my_printf("Hello World");
+
+    return 0;
+}
 
 
